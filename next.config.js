6b02289f14a6ad08b78ccc136b298be1295bc727
MikeyOnyedika <michaelvik520@gmail.com>
2023-12-14 20:04:11 +0100

    change remote strapi API url to localhost

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: "https",
-                hostname: "picacheese-backend.onrender.com",
-                port: "",
+                protocol: "http",
+                hostname: "localhost",
+                port: "1337",
                 pathname: "/**",
             }
         ]
